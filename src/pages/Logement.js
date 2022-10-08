7a790ac72516168846d413a5ca81038597a2ec6e
“[frebet <[frebet.julien@gmail.com]>
2022-10-08 18:45:29 +0200

    page logement statique ok

--- src/pages/Logement.js
+++ src/pages/Logement.js
@@ -62,14 +62,14 @@ function Logement() {
             <div class="top_right">
               <div class="top_host">
                 <div class="top_host_name">
-                  <p>Alex</p>
+                  <p>Alexandre</p>
                   <p>Dumas</p>
                 </div>
-                <div>
+                <div class="top_host_img">
                   <img></img>
                 </div>
               </div>
-              <div class="middle_ratings">
+              <div class="top_right_ratings">
                 <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M18.645 12L15 0L11.355 12H0L9.27 18.615L5.745 30L15 22.965L24.27 30L20.745 18.615L30 12H18.645Z" fill="#FF6060" />
                 </svg>
@@ -90,24 +90,38 @@ function Logement() {
           </div>
           <div class="bottom">
             <div class="bottom_description">
-              <p>description</p>
+              <div class="bottom_description_title">
+                <p>Description</p>
+                <svg width="28" height="17" viewBox="0 0 28 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M24.84 16.3466L27.2 13.9733L14 0.786621L0.800025 13.9866L3.16003 16.3466L14 5.50662L24.84 16.3466Z" fill="white" />
+                </svg>
+              </div>
               <div class="bottom_description_texte">
-                Profitez du charme de la vie parisienne dans un magnifique appartement. A 3 minutes à pied du Canl Saint Martin, vous serez proche des
-                transports, mais également de nombreux commerces. L'appartement est tout équipé, et possède également un parking pour ceux qui
-                souhaitent se déplacer en voiture.
+                <p>
+                  Profitez du charme de la vie parisienne dans un magnifique appartement. A 3 minutes à pied du Canal Saint Martin, vous serez proche
+                  des transports, mais également de nombreux commerces. L'appartement est tout équipé, et possède également un parking pour ceux qui
+                  souhaitent se déplacer en voiture.
+                </p>
               </div>
             </div>
-            <div class="bottom_equipement">
-              <p>Equipement</p>
-              <ul>
-                <li>Climatisation</li>
-                <li>Wi-fi</li>
-                <li>Cuisine</li>
-                <li>Espace de travail</li>
-                <li>Fer a repasser</li>
-                <li>Seche-cheveux</li>
-                <li>Cintres</li>
-              </ul>
+            <div class="bottom_description">
+              <div class="bottom_description_title">
+                <p>Equipement</p>
+                <svg width="28" height="17" viewBox="0 0 28 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M24.84 16.3466L27.2 13.9733L14 0.786621L0.800025 13.9866L3.16003 16.3466L14 5.50662L24.84 16.3466Z" fill="white" />
+                </svg>
+              </div>
+              <div class="bottom_description_texte">
+                <ul>
+                  <li>Climatisation</li>
+                  <li>Wi-fi</li>
+                  <li>Cuisine</li>
+                  <li>Espace de travail</li>
+                  <li>Fer a repasser</li>
+                  <li>Seche-cheveux</li>
+                  <li>Cintres</li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
